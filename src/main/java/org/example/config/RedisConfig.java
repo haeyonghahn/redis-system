60698d390686745d428a743e00bc7160901e72fe
yong7317 <yong7317@douzone.com>
2024-08-19 11:26:02 +0900

    Fix: redis sentinel 구성

--- src/main/java/org/example/config/RedisConfig.java
+++ src/main/java/org/example/config/RedisConfig.java
@@ -1,9 +1,10 @@
 package org.example.config;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.RedisSentinelConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -12,21 +13,50 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.redis.password}")
+    private String password;
+
+    @Value("${spring.redis.sentinel.master}")
+    private String master;
+
+    @Value("${spring.redis.sentinel.password}")
+    private String sentinelPassword;
+
+    @Value("${spring.redis.sentinel.nodes:#{null}}")
+    private String nodes;
+
     @Bean
     public RedisTemplate<?, ?> redisCacheTemplate(
-        @Qualifier("lettuceConnectionFactory") LettuceConnectionFactory lettuceConnectionFactory,
-        @Qualifier("stringRedisSerializer") StringRedisSerializer stringRedisSerializer) {
+        @Qualifier("sentinelConnectionFactory") LettuceConnectionFactory lettuceConnectionFactory,
+        @Qualifier("stringRedisSerializer") StringRedisSerializer stringRedisSerializer,
+        @Qualifier("jsonSerializer") GenericJackson2JsonRedisSerializer jsonSerializer) {
         RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(lettuceConnectionFactory);
         redisTemplate.setKeySerializer(stringRedisSerializer);
-        redisTemplate.setValueSerializer(jsonSerializer());
+        redisTemplate.setValueSerializer(jsonSerializer);
         redisTemplate.setHashKeySerializer(stringRedisSerializer);
         return redisTemplate;
     }
 
-    @Bean(name = "lettuceConnectionFactory")
-    public LettuceConnectionFactory lettuceConnectionFactory() {
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("127.0.0.1", 6379));
+    private LettuceConnectionFactory lettuceConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean(name = "sentinelConnectionFactory")
+    public LettuceConnectionFactory sentinelConnectionFactory() {
+        RedisSentinelConfiguration redisSentinelConfiguration = new RedisSentinelConfiguration().master(master);
+        if (nodes != null && !nodes.isEmpty()) {
+            String[] sentinel = nodes.split(",");
+            for (String node : sentinel) {
+                String[] s = node.split(":");
+                String host = s[0].trim();
+                int port = Integer.parseInt(s[1].trim());
+                redisSentinelConfiguration.sentinel(host, port);
+            }
+            redisSentinelConfiguration.setSentinelPassword(sentinelPassword);
+            redisSentinelConfiguration.setPassword(password);
+        }
+        return new LettuceConnectionFactory(redisSentinelConfiguration);
     }
 
     @Bean(name = "jsonSerializer")
